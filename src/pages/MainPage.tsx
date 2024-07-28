9fd8dbe6d6bf0eac428238e1ac8831d45dfb5ac2
ZubkovE <ezubcow@gmail.com>
2024-07-28 17:20:05 +0300

    fix interface 6

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -30,11 +30,11 @@ const MainPage = () => {
     return (
         <>
             <Modal {... { isMounted }} />
-            <div className="main-page h-full flex flex-col items-center px-[10px]">
-                <h1 className="text-3xl mt-[16px]">МОЯ ПЛАНЕТА</h1>
+            <div className="main-page h-full flex flex-col items-center">
+                <h1 className="text-3xl mt-[3vh]">МОЯ ПЛАНЕТА</h1>
                 <div className="flex flex-col items-center relative">
                     <h2 className="min-w-[300px] text-2xl text-start"> 100 000 </h2>
-                    <div className="min-w-[300px] border-2 rounded-r-[30px] rounded-bl-[30px] h-[108px] px-[30px] pt-[24px]">
+                    <div className="min-w-[300px] border-2 rounded-r-[30px] rounded-bl-[30px] h-[17vh] px-[30px] pt-[3vh]">
                         <div className="flex justify-between">
                             <h1 className="h-max">УРОВЕНЬ <span className="font-['Cydre']">2</span></h1>
                             <div className="flex h-max align-center">
@@ -45,20 +45,20 @@ const MainPage = () => {
                             </div>
                         </div>
 
-                        <div className="border-2 border-white overflow-hidden rounded-r-[30px] rounded-bl-[30px] h-[20px] mt-[5px]">
+                        <div className="border-2 border-white overflow-hidden rounded-r-[30px] rounded-bl-[30px] h-[3vh] mt-[1vh]">
                             <span className={`block rounded-r-[30px] h-full bg-white w-1/5`}></span>
                         </div>
                     </div>
                 </div>
-                <div className="w-full flex justify-center relative bottom-[20px] z-1">
-                    <div className="planet rounded-full h-[210px] w-[210px]">
+                <div className="w-full flex justify-center relative bottom-[3vh] z-1">
+                    <div className="planet rounded-full h-[36vh] w-[36vh]">
                     </div>
                 </div>
                 <div className="flex flex-col items-center relative bottom-[12px]">
                     <div className="text-center leading-4 text-sm max-w-[300px]">
                         Приглашай друзей! Чем больше друзей вы пригласите, тем выше будет уровень вашей планеты
                     </div>
-                    <div className="min-w-[300px] invite-friends-block mt-[5px] border-0 rounded-r-[20px] rounded-bl-[20px] h-[116px] px-[30px] pt-[20px]">
+                    <div className="min-w-[300px] invite-friends-block mt-[1vh] border-0 rounded-r-[20px] rounded-bl-[20px] h-[18vh] px-[30px] pt-[3vh]">
                         <button className={`border-b-2 text-sm bg-transparent w-full text-start flex justify-between h-max overflow-hidden px-1 transition delay-200 ${isCopied ? ("border-[#81B94A] text-[#81B94A]") : ("border-[#A8A8A8] text-[#A8A8A8]")}`}
                             onClick={() => {
                                 copyTextToClipboard(`https://t.me/OpenSpaceAI_bot?start=${Telegram.WebApp.initDataUnsafe.user?.username}`);
@@ -72,12 +72,12 @@ const MainPage = () => {
                             </div>
                             <img src={copyIcon} className={`transition delay-200 ${isCopied ? "green-copy" : "gray-copy"}`} />
                         </button>
-                        <div className="leading-[17.6px] text-sm mt-[5px]">Повышение уровня: <br />
+                        <div className="leading-[17.6px] text-sm mt-[1vh]">Повышение уровня: <br />
                             5 приглашенных друзей</div>
                     </div>
                     <button
                         type="button"
-                        className="relative bottom-[26px] h-[50px] w-[230px] rounded-[15px] bg-[#4A74B9] text-base transition delay-200 active:bg-[#7C95BF]"
+                        className="relative bottom-[4vh] h-[8vh] w-[230px] rounded-[15px] bg-[#4A74B9] text-base transition delay-200 active:bg-[#7C95BF]"
                         onClick={() => { Telegram.WebApp.openTelegramLink(`https://t.me/share/url?url=https://t.me/OpenSpaceAI_bot?start=${Telegram.WebApp.initDataUnsafe.user?.username}&text=${encodeURI(ruText)}`) }}
                     >Пригласить друзей</button>
                 </div>
--- src/pages/Modal.tsx
+++ src/pages/Modal.tsx
@@ -13,13 +13,13 @@ const Modal = (props: modalFlag) => {
         props.isMounted
             ? (<Portal id={MODAL_CONTAINER_ID}>
                 <div className='fixed flex-col flex-nowrap top-0 w-full h-full bg-[#1E1E1ECC]'>
-                    <div className={`fixed w-[300px] h-[146px] bg-white z-200 modalActive rounded-[30px]`}>
+                    <div className={`fixed w-[300px] h-[26vh] bg-white z-200 modalActive rounded-[30px]`}>
                         <div className='ralative w-full h-full p-[20px] flex-col text-center'>
-                            <h2 className='text-black text-sm mb-2'>
+                            <h2 className='text-black text-base mb-2'>
                                 Планета недоступна, если ты не подписан на наш канал
                             </h2>
                             <button type="button" 
-                            className="relative text-base h-[60px] w-[247px] rounded-[15px] bg-[#4A74B9] transition delay-200 active:bg-[#7C95BF]"
+                            className="relative text-base h-[10vh] w-[247px] rounded-[15px] bg-[#4A74B9] transition delay-200 active:bg-[#7C95BF]"
                             onClick={() => Telegram.WebApp.openTelegramLink('https://t.me/openspaceaim')}
                             >Подписаться</button>
                         </div>
