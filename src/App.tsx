76169a5da6016811cc85122094a419fb212af35f
ZubkovE <ezubcow@gmail.com>
2024-08-02 13:46:41 +0300

    prod fix 5

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function App() {
     tg.expand();
     tg.disableVerticalSwipes();
     console.log(tg.platform)
-    if (tg.platform === 'tdesktop' || tg.platform === 'macos' || tg.platform === 'weba' || tg.platform === 'webk' || tg.platform === 'unknown') {
+    if (tg.platform === 'tdesktop' || tg.platform === 'macos' || tg.platform === 'weba' || tg.platform === 'web' || tg.platform === 'webk' || tg.platform === 'unknown') {
       setBlock(true);
     } else {
       getAll();
