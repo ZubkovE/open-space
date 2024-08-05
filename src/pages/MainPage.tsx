b7ea5ecf0d8cc4f0748505f441a3d2dd6c715b5d
ZubkovE <ezubcow@gmail.com>
2024-08-05 20:48:56 +0300

    final version 2

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -7,9 +7,7 @@ import { modalFlag } from "../api/models/modalType"
 import { MainPageInterface } from "../api/models/mainPageInterface"
 
 
-const ruText = '👆Твое приглашение здесь\n' +
-    '\n' +
-    'Лови билет в космос, скоро взлетаем 🌞🚀'
+const ruText = '\n Лови билет в космос, скоро взлетаем 🌞🚀'
 
 const copyTextToClipboard = async (text: string) => {
     try {
